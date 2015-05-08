009c7b3e1db68b2bec3215ad228045188f014015
IamKenshin <ryantruong1993@gmail.com>
2015-05-08 10:04:51 -0700

    05/08/2015

--- src/Pit.java
+++ src/Pit.java
@@ -1,7 +1,11 @@
+import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
-public abstract class Pit
+import javax.swing.JComponent;
+
+public abstract class Pit extends JComponent
 {
+	private Ellipse2D pitShape;
 	/**
 	 * I'm not sure if the pits should store count, or if the model should...
 	 * @return
