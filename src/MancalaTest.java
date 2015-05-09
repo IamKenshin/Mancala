a34d4632ea8b61ae60f725bccafe33f3733e076f
ladimer <ladimer@users.noreply.github.com>
2015-05-09 10:38:14 -0700

    Update MancalaTest.java

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -2,16 +2,31 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class MancalaTest
+
+
 {
+	public static void modelCaller (int a){
+		
+		Model model = new Model(a);
+		
+		new Board(model);
+	}
+	
 	public static void main(String[] args)
 	{
+		Board b = new Board();
 		try {UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}
 		catch (ClassNotFoundException e) {}
 		catch (InstantiationException e) {}
 		catch (IllegalAccessException e) {}
 		catch (UnsupportedLookAndFeelException e) {}
 		
-		Model model = new Model(3);
+		Model model = new Model();
+		
 		new Board(model);
+		b.initializeCenter();// we need to call the actionPerformed method here i think. 
+		// and then we need to call the above method : modelCaller() 
+		// and then we can be set. 
+		
 	}
 }
