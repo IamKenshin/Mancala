cb4e8619fc29e40a85f5cfcf3573e877ed3b2b0b
dandroid126 <dan.collins126@gmail.com>
2015-05-08 17:03:44 -0700

    Marble
    Marble is being edited currently It is not finished.

--- src/Marble.java
+++ src/Marble.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 
 /**
  * The only purpose of this class is to describe what a marble looks like.
@@ -7,8 +8,20 @@ import java.awt.Graphics2D;
  */
 public class Marble
 {
+	int width;
+	int height;
+	int x;
+	int y;
+	public Marble(int count)
+	{
+		width = 10;
+		height = 10;
+		x = 0; //these will be changed
+		y = 0; //these will be changed
+	}
+
 	public void draw(Graphics2D g2)
 	{
-		
+		g2.fill(new Ellipse2D.Double(x, y, width, height));
 	}
 }
